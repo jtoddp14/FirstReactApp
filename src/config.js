225ee45e6b7e1b00b2b683191cd52fa839429f0e
jtoddp14 <36805592+jtoddp14@users.noreply.github.com>
2019-04-14 23:15:37 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "sdfsfgsdfgsd",
+      BUCKET: "sdfgsdfgsdfg"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "sdfgsdfgsdfgsdfg"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "sdfgsdfgsdfgsdfg",
+      APP_CLIENT_ID: "sfgsdfgsdfgsdfg",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };
+
